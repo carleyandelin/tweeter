b1ca525a9caaba6e96ac2be8700fd7a2bc155667
carleyandelin <carleyna@byu.edu>
2026-03-01 22:58:08 -0700

    changed PostStatusPresenter to use parents

--- tweeter-web/src/presenter/PostStatusPresenter.ts
+++ tweeter-web/src/presenter/PostStatusPresenter.ts
@@ -1,8 +1,9 @@
 import { AuthToken, Status, User } from "tweeter-shared";
 import { StatusService } from "../model.service/StatusService";
+import { Presenter, View } from "./Presenter";
 
 
-export interface PostStatusView {
+export interface PostStatusView extends View {
     setIsLoading: (value: boolean) => void
     displayErrorMessage: (message: string) => void
     displayInfoMessage: (message: string, duration: number, bootstrapClasses?: string | undefined) => string
@@ -10,43 +11,33 @@ export interface PostStatusView {
     deleteMessage: (message: string) => void
 }
 
-export class PostStatusPresenter {
-    _view: PostStatusView
+export class PostStatusPresenter extends Presenter<PostStatusView> {
     service: StatusService
 
     public constructor(view: PostStatusView) {
-        this._view = view;
+        super(view);
         this.service = new StatusService();
     }
 
     public async submitPost(event: React.MouseEvent, post: string, currentUser: User, authToken: AuthToken) {
         event.preventDefault();
-
         var postingStatusToastId = "";
-
-        try {
-            this._view.setIsLoading(true);
-            postingStatusToastId = this._view.displayInfoMessage(
+        this.doFailureReportingOperation(async () => {
+            this.view.setIsLoading(true);
+            postingStatusToastId = this.view.displayInfoMessage(
                 "Posting status...",
                 0
             );
-
             const status = new Status(post, currentUser!, Date.now());
 
             await this.service.postStatus(authToken, status)
 
-            this._view.setPost("");
-            this._view.displayInfoMessage("Status posted!", 2000);
-        } catch (error) {
-            this._view.displayErrorMessage(
-                `Failed to post the status because of exception: ${error}`
-            );
-        } finally {
-            this._view.deleteMessage(postingStatusToastId);
-            this._view.setIsLoading(false);
-        }
-  };
-
-
+            this.view.setPost("");
+            this.view.displayInfoMessage("Status posted!", 2000);
+        }, "post the status")
+        // finally
+        this.view.deleteMessage(postingStatusToastId);
+        this.view.setIsLoading(false);
+    };
 
 }

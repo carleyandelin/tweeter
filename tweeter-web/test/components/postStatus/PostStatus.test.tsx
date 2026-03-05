b36b97855ab1aa7240f916d96bbae605be30fbb3
carleyandelin <carleyna@byu.edu>
2026-03-04 20:12:25 -0700

    added base PostStatus.test.tsx file

---
+++ tweeter-web/test/components/postStatus/PostStatus.test.tsx
@@ -0,0 +1,11 @@
+
+
+describe("PostStatus component", () => {
+    it("starts with the post status and clear buttons are disabled", () => {})
+
+    it("enables both buttons when the text field has text", () => {})
+
+    it("disables both buttons when the text field has been cleared", () => {})
+
+    it("calls the presenters postStatus method with correct parameters when the post status button is pressed", () => {})
+});
